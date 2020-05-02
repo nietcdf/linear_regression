44d58a0653f26c5ec63f72c6369eb778c21bf796
Matthew Welty <reidwelty@gmail.com>
2020-05-02 17:50:37 -0500

    updated plot.py it now creates a plot for a predefined data set in a txt file

--- plot.py
+++ plot.py
@@ -1,4 +1,30 @@
-import matplot.pyplot as plt
+import matplotlib.pyplot as plt
 import sys
+import csv
+import numpy as np
 
 data_file = sys.argv[1]
+
+## plot prefit data
+
+#data_str = np.array([])
+#np.append(data,1)
+data_str = []
+
+
+with open('ex1data1.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')    
+    for row in csv_reader:   
+        data_str.append(row)
+        
+data=np.asarray(data_str).astype(np.float)
+#print(data)
+#print(data[0])
+#print(data.shape)
+#print(data[:,1])
+
+x = data[:,0]
+y = data[:,1]
+
+plt.scatter(x,y,marker="x",c="red")
+plt.show()
