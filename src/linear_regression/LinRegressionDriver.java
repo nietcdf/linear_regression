6f3c5c62be350cca42dfa5901f2e2606a16100bf
Matthew Welty <reidwelty@gmail.com>
2020-05-03 01:44:04 -0500

    wrote javadocs

--- src/linear_regression/LinRegressionDriver.java
+++ src/linear_regression/LinRegressionDriver.java
@@ -10,12 +10,30 @@ import java.util.*;
 import java.io.*;
 
 
+/**
+ * @author matthew_welty
+ * This class has an example linear regression on a set of cubic data
+ * it also contains several methods to read in files and get the data into a useful format
+ */
 public class LinRegressionDriver {
 	
+	/**
+	 * @param filename name of the file to be read in without skipping the first line of the file
+	 * @return each line of the file put into the arraylist
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
 	public static ArrayList<String> read(String filename) throws FileNotFoundException,IOException{
 		return read(filename,false);
 	}
 	
+	/**
+	 * @param filename name of the file to be read in
+	 * @param skipFirst specify if you want to skip the first line of the file
+	 * @return each line of the file put into the arraylist
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
 	public static ArrayList<String> read(String filename, boolean skipFirst) throws FileNotFoundException,IOException{
 		BufferedReader reader = new BufferedReader(new FileReader(filename));
 		ArrayList<String> lines = new ArrayList<String>();
@@ -34,6 +52,12 @@ public class LinRegressionDriver {
 		return lines;
 	}
 	
+	/**
+	 * @param numStrs Array of strings to be split
+	 * @param delimeter specifies what character/string to strings in the array will be split by
+	 * @return 2d array of double where the strings in the numStr arrayList
+	 * have been split according to the delimeter and converted to doubles
+	 */
 	public static double[][] readStrdouble2D(ArrayList<String> numStrs,String delimeter){
 		double[][] nums2D = new double[numStrs.size()][/*(numStrs.get(0).length()+1)/2*/];
 		String tempStr = "";
@@ -53,6 +77,13 @@ public class LinRegressionDriver {
 		return nums2D;
 	}
 	
+	/**
+	 * @param numStrs Array of strings to be split
+	 * @param delimeter specifies what character/string to strings in the array will be split by
+	 * @param removeFirst specifies if you will remove the first column from the output
+	 * @return 2d array of double where the strings in the numStr arrayList
+	 * have been split according to the delimeter and converted to doubles
+	 */
 	public static double[][] readStrdouble2D(ArrayList<String> numStrs,String delimeter, boolean removeFirst){
 		double[][] nums2D = new double[numStrs.size()][/*(numStrs.get(0).length()+1)/2*/];
 		String tempStr = "";
@@ -75,10 +106,21 @@ public class LinRegressionDriver {
 		return nums2D;
 	}
 	
+	/**
+	 * @param filename file to read in data from
+	 * @param delimeter specifies how each line is split
+	 * @return data from file as 2D double array
+	 * @throws IOException
+	 * @throws FileNotFoundException
+	 */
 	public static double[][] read2D(String filename,String delimeter) throws IOException,FileNotFoundException{
 		return readStrdouble2D(read(filename),delimeter);
 	}
 
+	/**
+	 * @param data removes first element from array
+	 * @return data without first element
+	 */
 	public static double[] removeFirst (double[] data) {
 		double[] newData = new double[data.length-1];
 		for(int i=1; i<data.length;i++) {
@@ -90,6 +132,10 @@ public class LinRegressionDriver {
 		return newData;
 	}
 	
+	/**
+	 * @param data removes first column from 2D array
+	 * @return data without first column
+	 */
 	public static double[][] removeFirst(double[][] data){
 		double[][] newData = new double[data.length][];
 		for(int i=0;i<data.length;i++) {
@@ -98,6 +144,12 @@ public class LinRegressionDriver {
 		return newData;
 	}
 	
+	/**
+	 * @param filename csv file to be read in
+	 * @return csv data converted to 2D double array
+	 * @throws IOException
+	 * @throws FileNotFoundException
+	 */
 	public static double[][] readCSV(String filename) throws IOException,FileNotFoundException{
 		return removeFirst(readStrdouble2D(read(filename,true),",",true));
 	}
@@ -106,21 +158,21 @@ public class LinRegressionDriver {
 		// TODO Auto-generated method stub
 		
 
-		String cubic_file = "cubic.csv";
-		Update cubicReg = new Update(cubic_file,3);
-		cubicReg.setAlpha(.0001);
-		cubicReg.setIterations(10000);
-		cubicReg.iterateTheta();
+		String cubic_file = "cubic.csv";//input csv file name
+		Update cubicReg = new Update(cubic_file,3);//3 is the degree of polynomial to be fit
+		cubicReg.setAlpha(.0001); //fit was diverging with default alpha
+		cubicReg.setIterations(10000); //increased iterations to improve fit
+		cubicReg.iterateTheta(); //actually calling update method
 		
 		
-		String outData = "cubicReg.txt";
-		String outPred = "cubicRegPrediction.txt";
-		cubicReg.writeData(outData);
-		cubicReg.writePrediction(outPred);
+		String outData = "cubicReg.txt"; //output file name for the original data set with theta paramaters
+		String outPred = "cubicRegPrediction.txt";// output file name for the predicted value of y for all the input data
+		cubicReg.writeData(outData); //write the data to file
+		cubicReg.writePrediction(outPred);//write the predictions to file
 		
-		String pythonScript = "./plot.py";
-		String commandStr = pythonScript+" "+outData + " " +outPred;
-		Process process = Runtime.getRuntime().exec(commandStr);
+		String pythonScript = "./plot.py";//the python executable to be called
+		String commandStr = pythonScript+" "+outData + " " +outPred;//command to be called
+		Process process = Runtime.getRuntime().exec(commandStr);//running the python executable with correct input file names
 	}
 
 }
--- src/linear_regression/Update.java
+++ src/linear_regression/Update.java
@@ -1,3 +1,5 @@
+
+
 package linear_regression;
 import java.lang.Math;
 import java.io.BufferedWriter;
@@ -8,8 +10,35 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author matthew_welty
+ * This class allows you to create an Update object which you can then call to do 
+ * iterative updates to your parameters which will begin to give you a fit to the data
+ */
+
+
+
 public class Update {
 	//cost function for all training examples
+	/**
+	 * each row of x corresponds to 1 training example, so you can have any number of 
+	 * paramaters that you want
+	 * 
+	 * each element of y corresponds to 1 training example
+	 * 
+	 * theta is the array of the fitting paramaters
+	 * 
+	 * alpha is the step size of iterations
+	 * 
+	 * x_bias is the same as the x matrix but with a column of 1's prepended to x,
+	 * so that we can have an intercept term in our fit
+	 * 
+	 * m is the number of training examples = y.length and x.length
+	 * 
+	 * costHistory contains the costFunction history as a function of iteration#, if the 
+	 * fit is going well the cost should decrease with every iteration, if the cost is increasing
+	 * the fit may begin to diverge
+	 */
 	double[][] x;
 	double[] y;
 	double[] theta;
@@ -21,11 +50,22 @@ public class Update {
 	
 	//filename corresponds to a CSV file in the format output by R, 
 	//the first row is column headers, the first column is row indices
+	/**
+	 * @param filename csv filename, format for the csv should be in the format output by R,
+	 * the first row is column headers, the first column is row indices
+	 * @param n the degree of the polynomial fit
+	 * @throws IOException
+	 * @throws FileNotFoundException
+	 */
 	public Update(String filename,int n) throws IOException,FileNotFoundException{
 		this(LinRegressionDriver.readCSV(filename),n);
 	}
 	
 	//creates Update object for data and fitting as a n degree polynomial
+	/**
+	 * @param data 2D array where the last column is the y values
+	 * @param n the degree of the polynomial fit
+	 */
 	public Update(double[][] data, int n) {
 		double[][] xTerms = Update.splitX(data);
 		double[] yTerms = Update.splitY(data);
@@ -39,6 +79,10 @@ public class Update {
 	}
 	
 	//each row of x should be a training example
+	/**
+	 * @param x data points which will be fit to y
+	 * @param y values to be fit to
+	 */
 	public Update(double[][] x, double[] y) {
 		this.x = x;
 		this.y = y;
@@ -47,6 +91,10 @@ public class Update {
 		this.m = x.length;
 	}
 	
+	
+	/**
+	 * @param data 2D array where the last column is the y values
+	 */
 	public Update(double[][] data) {
 		assignXY(data);
 		this.x_bias = insertBias(x);
@@ -54,14 +102,26 @@ public class Update {
 		this.m = x.length;
 	}
 	
+	/**
+	 * @param alpha this determines the step size of the updates to the thetas,
+	 * if the fit diverges decrease alpha
+	 */
 	public void setAlpha(double alpha) {
 		this.alpha = alpha;
 	}
 	
+	
+	/**
+	 * @param iter sets the iteration number for the object
+	 */
 	public void setIterations(int iter) {
 		this.iterations = iter;
 	}
 	
+	/**
+	 * @param iter the number of updates to perform on the thetas,
+	 * if the fit is converging but doesn't appear to have reached a minimum, increase iter
+	 */
 	public void iterateTheta(int iter) {
 		for(int i=0;i<iter;i++) {
 			updateThetas();
@@ -70,11 +130,18 @@ public class Update {
 		}
 	}
 	
+	
+	/**
+	 * performs the default number of iterations
+	 */
 	public void iterateTheta() {
 		iterateTheta(iterations);
 	}
 	
 	//Assign y to the last column of the data and x as the rest
+	/**
+	 * @param data assigns the last column to Y and the rest as X
+	 */
 	private void assignXY(double[][] data) {
 		double[][] trans = Update.transpose(data);
 		this.y = trans[trans.length-1];
@@ -88,6 +155,11 @@ public class Update {
 		this.x = Update.transpose(trans_noY);
 	}
 	
+	
+	/**
+	 * @param data input data containing both x and y
+	 * @return returns data containing only x values
+	 */
 	public static double[][] splitX(double[][] data){
 		double[][] trans = Update.transpose(data);
 		//this.y = trans[trans.length-1];
@@ -101,11 +173,19 @@ public class Update {
 		return Update.transpose(trans_noY);
 	}
 	
+	/**
+	 * @param data input data containing both x and y
+	 * @return returns data containing only y values
+	 */
 	public static double[] splitY(double[][] data){
 		double[][] trans = Update.transpose(data);		
 		return trans[trans.length-1];
 	}
 	
+	/**
+	 * @return cost calculated for current set of x,y,theta values
+	 * updates costHistory
+	 */
 	public double cost() {
 		double cost = cost(x_bias,y,theta);
 		costHistory.add(cost);
@@ -113,6 +193,12 @@ public class Update {
 	}
 	
 	//
+	/**
+	 * @param x values with bias term
+	 * @param y values 
+	 * @param theta parameters being fit
+	 * @return the cost of the fit with current set of thetas
+	 */
 	public double cost(double[][] x, double[] y, double[] theta) {
 		double sum = 0;
 		double[] inner = hypothesisAll_minus_y(x, y, theta);
@@ -122,6 +208,11 @@ public class Update {
 		return sum/(2*m);
 	}
 	
+	/**
+	 * @param x values with bias term
+	 * @param y values
+	 * @param theta values to be updated
+	 */
 	public void updateThetas(double[][] x, double[] y, double[] theta) {
 		double[] h_minus_y = hypothesisAll_minus_y(x, y, theta);
 		double[] updatedTheta = new double[theta.length];
@@ -133,11 +224,20 @@ public class Update {
 		this.theta = updatedTheta;	
 	}
 	
+	/**
+	 * updates current set of thetas based on current state of the system
+	 */
 	public void updateThetas() {
 		updateThetas(x_bias, y, theta);
 	}
 	
-	//used x_bias
+	
+	/**
+	 * @param h_minus_y hypothesis function - y value for each training example
+	 * @param x x_bias value in usual format
+	 * @param thetaIndex the index of the theta being updated
+	 * @return the gradient of our theta at theta index
+	 */
 	public double[] updateThetaInner(double[] h_minus_y, double[][] x, int thetaIndex) {
 		double[] inner = new double[h_minus_y.length];
 		for(int i=0;i<h_minus_y.length;i++) {
@@ -146,6 +246,10 @@ public class Update {
 		return inner;
 	}
 	
+	/**
+	 * @param data 
+	 * @return the sum of data
+	 */
 	private double sum1D(double[] data) {
 		double sum = 0;
 		for(double d: data) {
@@ -154,7 +258,13 @@ public class Update {
 		return sum;
 	}
 	
-	//hypothesis for all training examples - y value for the corresponding example
+	
+	/**
+	 * @param x the x_bias value
+	 * @param y the usual y values
+	 * @param theta fitting parameters
+	 * @return the hypothesis value - y for all training examples
+	 */
 	public double[] hypothesisAll_minus_y(double[][] x, double[] y, double[] theta) {
 		double h_all[] = new double[x.length];
 		for(int i=0; i<x.length;i++) {
@@ -163,6 +273,12 @@ public class Update {
 		return h_all;
 	}
 	
+	/**
+	 * @param x x_bias for single example
+	 * @param y for single example
+	 * @param theta fitting parameters
+	 * @return the hypothesis value - y for a single training example
+	 */
 	public double hypothesis_minus_y(double[] x, double y, double[] theta) {
 		double h = 0;
 		for(int i=0; i<x.length;i++) {
@@ -171,6 +287,11 @@ public class Update {
 		return h-y;
 	}
 	
+	/**
+	 * @param x single example to predict the value of 
+	 * @param theta fitted parameters
+	 * @return the predicted value of x using the fit
+	 */
 	public double predict(double[] x, double[] theta) {
 		double h = 0;
 		for(int i=0; i<x.length;i++) {
@@ -180,12 +301,20 @@ public class Update {
 	}
 	
 	
-	//use x_bias (for single example just have 1 prepended)
+	
+	/**
+	 * @param x_bias x values for a single example with bais element
+	 * @return predicted value for a single example
+	 */
 	public double predict(double[] x_bias) {
 		return predict(x_bias,theta);
 	}
 	
 	//x_bias
+	/**
+	 * @param x_bias each row is a training example to calculate predicted value
+	 * @return predicted value for each example in x_bias
+	 */
 	public double[] predict(double[][] x_bias) {
 		double[] h = new double[x_bias.length];
 		for(int i=0; i<h.length;i++) {
@@ -195,6 +324,13 @@ public class Update {
 		
 	}
 	//calculates the values of the model between max and min for polynomial of degree n
+	/**
+	 * @param max value of range of values to be predicted
+	 * @param min value of range of values to be predicted
+	 * @param n degree of polynomial
+	 * @param numPoints number of points to predict between max and min
+	 * @return predicted value of points between max and min
+	 */
 	public double[] predictPoly(double max, double min, int n, int numPoints) {
 		double[] range = new double[numPoints];
 		for(int i=0; i<numPoints;i++) {
@@ -206,6 +342,10 @@ public class Update {
 	}
 	
 	//adds a column of 1's to the beggining of the matrix
+	/**
+	 * @param x arbitrary 2D matrix
+	 * @return original matrix with column of 1's prepended
+	 */
 	public double[][] insertBias(double[][] x) {
 		double[][] x_bias = new double[x.length][x[0].length+1];
 		for(int i = 0;i<x_bias.length;i++) {
@@ -222,6 +362,10 @@ public class Update {
 	}
 	
 	//returns transpose of 2D double array
+	/**
+	 * @param data all the matrix rows should be the same size and same for columns
+	 * @return transpose of the matrix
+	 */
 	public static double[][] transpose(double[][] data){
 		double[][] newData = new double[data[0].length][data.length];
 		for (int i=0;i<data.length;i++) {
@@ -232,6 +376,11 @@ public class Update {
 		return newData;
 	}
 	
+	/**
+	 * @param data double values to be printed
+	 * @param delimeter string to be interjected between double values
+	 * @return
+	 */
 	public static String print1D(double[] data,String delimeter) {
 		String master = "";	
 		for(int i =0; i<data.length; i++) {
@@ -246,6 +395,12 @@ public class Update {
 		return master;
 	}
 	
+	/**
+	 * @param data values to be converted to a string
+	 * @param delimeter1 each row is separated by this
+	 * @param delimeter2 each row element is separated by this
+	 * @return
+	 */
 	public static String print2D(double[][] data, String delimeter1,String delimeter2) {
 		String master = "";
 		for(int i =0; i<data.length; i++) {
@@ -260,26 +415,48 @@ public class Update {
 		return master;
 	}
 	
+	/**
+	 * @param data values to be converted to a string
+	 * @return each datum separated by a newline
+	 */
 	public static String print1D(double[] data) {
 		return print1D(data,"\n");
 	}
 	
+	/**
+	 * @param data values to be converted to a string
+	 * @return each element in a row is separated by a comma, each row by a newline
+	 */
 	public static String print2D(double[][] data) {
 		return print2D(data,"\n",",");
 	}
 	
+	/**
+	 * @return x values with bias as a string
+	 */
 	public String xStr() {
 		return print2D(x_bias);
 	}
 	
+	/**
+	 * @return y values as a string
+	 */
 	public String yStr() {
 		return print1D(y);
 	}
 	
+	/**
+	 * @return theta parameters as a string
+	 */
 	public String thetaStr() {
 		return print1D(theta,",");
 	}
 	
+	/**
+	 * @param x arbitrary 2D array with x.length = y.length
+	 * @param y array to be appended to x
+	 * @return x appended with y
+	 */
 	public double[][] append2D(double[][] x, double[] y){
 		double[][] all = new double[x.length][];
 		for(int i=0;i<x.length;i++) {
@@ -297,6 +474,12 @@ public class Update {
 		return all;
 	}
 	
+	/**
+	 * @param <type> dataType of the list
+	 * @param data to be converted into a string
+	 * @param delimeter value separating each datum
+	 * @return
+	 */
 	public static <type> String printList(List<type> data, String delimeter){
 		String master = "";	
 		for(int i =0; i<data.size(); i++) {
@@ -311,11 +494,19 @@ public class Update {
 		return master;
 	}
 	
+	/**
+	 * @return String version of costHistory
+	 */
 	public String costHistoryStr() {
 		return Update.printList(costHistory, "\n");
 	}
 	
 	//model the data as polynomial of degree n
+	/**
+	 * @param data datum to be raised to ith power where i is in the range (1,n)
+	 * @param n degree of polynomial
+	 * @return data raised to ith power where i is in the range (1,n)
+	 */
 	public static double[] model(double data,int n) {
 		if(n<=0) {
 			System.out.println("cannot accept value of n <= 0");
@@ -330,6 +521,11 @@ public class Update {
 		}
 	}
 	
+	/**
+	 * @param data each element is for a different training example
+	 * @param n polynomial power to fit to
+	 * @return 2D array with additional polynomial features added in
+	 */
 	public static double[][] model(double[] data, int n){
 		double[][] poly = new double[data.length][];
 		for(int i =0; i<data.length;i++) {
@@ -338,10 +534,20 @@ public class Update {
 		return poly;
 	}
 	
+	/**
+	 * @param data 2D array, 1D array nested inside 2D array
+	 * @param n polynomial to fit
+	 * @return elements in data have been raised to the power i up to n to create extra 
+	 * fitting features
+	 */
 	public static double[][] model(double[][] data, int n){
 		return model(transpose(data)[0],n);
 	}
 	
+	/**
+	 * @param filename output file name to put original data and thetas
+	 * @throws IOException
+	 */
 	public void writeData(String filename) throws IOException{
 		//write x, y, theta on top
 		String thetaStr = thetaStr();
@@ -350,6 +556,11 @@ public class Update {
 		save(filename,master);
 	}
 	
+	/**
+	 * @param filename prediction output file name
+	 * @throws IOException
+	 * writes predicted output for all input data to a file
+	 */
 	public void writePrediction(String filename) throws IOException{
 		String thetaStr = thetaStr();
 		double[] y_out = predict(x_bias);
@@ -358,6 +569,11 @@ public class Update {
 		save(filename,master);
 	}
 	
+	/**
+	 * @param filename to write output to
+	 * @param str everything to be written to the file as a single string
+	 * @throws IOException
+	 */
 	public static void save(String filename,String str) throws IOException {
 		BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
 		writer.write(str);
